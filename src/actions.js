7974db7308a5d17110ba156b7f110e4906e17750
arianna-h-h <ariannaharadon@gmail.com>
2017-08-31 09:19:21 -0500

    Implement store feature

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,3 @@
-
-
 export function togglePlayer () {
   return {
     type: 'TOGGLE_PLAYER'
@@ -17,4 +15,46 @@ export function updateBoard (position) { //i in handleClick?
     type: 'UPDATE_BOARD',
     position
   };
+}
+
+const initialState = {
+  history: [{
+    board: Array(9).fill(null)
+  }],
+  stepNumber: 0,
+  xIsNext: true,
+};
+
+// action = {
+//   type: 'UPDATE_BOARD',
+//   position: 0,
+// }
+
+export const ticTac = (state = initialState, action) => { //reducer
+  switch (action.type) {
+    case 'INCREMENT_STEP':
+      return {
+        ...state,
+        stepNumber: state.stepNumber + 1
+      }
+      case 'TOGGLE_PLAYER':
+        return {
+          ...state,
+          xIsNext: !state.xIsNext
+        }
+      case 'UPDATE_BOARD':
+        const position = action.position;
+        const currentPlayer = state.xIsNext ? 'X' : 'O';
+        const oldBoard = state.history[state.history.length - 1].board.slice();
+        oldBoard[position] = currentPlayer;
+        const newBoard = oldBoard;
+        const newHistory = state.history.concat([{ board: newBoard }]);
+
+        return {
+          ...state,
+          history: newHistory
+      }
+    default:
+      return state;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,97 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import './index.css';
 import {
   togglePlayer,
   incrementStep,
-  updateBoard
+  updateBoard,
+  ticTac
 } from './actions.js';
 
-const ticTac = (state = initialState, action) => { //reducer
-  switch (action.type) {
-    case 'INCREMENT_STEP':
-      return {
-        ...state,
-        stepNumber: state.stepNumber + 1
-      }
-      case 'TOGGLE_PLAYER':
-        return {
-          ...state,
-          xIsNext: !state.xIsNext
-        }
-      case 'UPDATE_BOARD':
-        return {
-          ...state,
-          history: state.history.concat([state.currentBoard])
-      }
-    default:
-      return state;
-  }
+function Square(props) { // all squares are rendered b4 here
+    console.log('top of square function')
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+      {console.log('inside square button' + props.onClick)}
+      {/* determines value of button */}
+    </button>
+  );
 }
 
-const store = createStore(ticTac);
-
-const initialState = {
-  history: [{
-    currentBoard: Array(9).fill(null)
-  }],
-  stepNumber: 0,
-  xIsNext: true,
-};
-
-store.dispatch(togglePlayer())
-store.dispatch(updateBoard(2))
-
-
-
-class Game extends React.Component {
-  handleClick(i) {
-    return (
-    store.dispatch(updateBoard(i))
-  )
-    //action dispatch
-  }
-  render() {
-    return (
-        <div className="game-board">
-          <div>Hello World</div>
-          {console.log(this.props.value)}
-                 {/* <Board
-                  // props
-                  onClick={i => this.handleClick(i)} // props
-                /> */}
-            </div>
-    );
-  }
-  }
-/*
-  function Square(props) {
-    // onClick="handleClick" -->
-    return ( // css class, passing in function to be used
-      <button className="square" onClick={props.onClick}>
-        {props.value}
-      </button>
-    );
-  }
-
-
-  class Board extends React.Component {
+class Board extends React.Component {
   renderSquare(i) {
+    {console.log('at render square')}
+    const currentBoard = this.props.squares;
+    const position = currentBoard[i]; // => 'X' || 'O' || null
     return (
       <Square
-        value={this.props.squares[i]} // props to square
-        onClickSquare={() => this.props.onClick(i)} // props
+        value={position} // determines x or O
+        onClick={() => this.props.onClick(i)} // handleClick in Game
       />
     );
   }
 
-  render() {
+  render() { //happens before render square
+      {console.log('at render in board')}
     return (
       <div>
         <div className="board-row">
+          {console.log('before br 0')}
           {this.renderSquare(0)}
+          {console.log('after br 0')}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
@@ -109,11 +59,49 @@ class Game extends React.Component {
     );
   }
 }
-*/
 
+class Game extends React.Component {
+  constructor (props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(i) { //passing correctly
+    console.log('at handleClick'); //only when something is clicked
+    store.dispatch(updateBoard(i))
+    store.dispatch(togglePlayer()) //switch player when clicked
+    store.dispatch(incrementStep())
+    //action dispatch
+  }
+  render() { //render happens before a click
+    const history = this.props.history.slice(0, this.props.stepNumber + 1); //returns a copy for no mutation
+    console.log("History: ", history);
+    const board = history[history.length - 1].board; //makes a copy of last board
+    //const { board } = current;
+    console.log(board);
+    return (
+      <div className="game">
+        <div className="game-board">
+                 <Board
+                  squares={board} //curent board (state in history)
+                  onClick={i => this.handleClick(i)} // position props
+                />
+            </div>
+      </div>
+    );
+  }
+}
+
+const store = createStore(ticTac);
+
+//fun activity: console.log state inside of render
 const render = () => {
+  const { history, xIsNext, stepNumber } = store.getState();
   ReactDOM.render(
-  <Game value={store.getState()} />,
+  <Game
+    history={history}
+    xIsNext={xIsNext}
+    stepNumber={stepNumber} />,
   document.getElementById('root')
   );
 }
